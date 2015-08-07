227dcd43f9a4f9af86d987e44a9f6c2fc0f6590d
hackmeopen <matt@hackmeopen.com>
2015-08-06 22:07:37 -0500

    Forgot to save files

--- codec.c
+++ codec.c
@@ -1,1 +1,10 @@
+/*codec.c
+ * Contains code related to the operation of a WM8731 stereo 24 bit codec.
+ * 
+ */
 
+void codec_init(void){
+    
+    //Initialize the codec via I2C and set up the SPI bus.
+    
+}
