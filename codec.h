1e13114197b9e0c3d895815d37448754932fb21d
hackmeopen <matt@hackmeopen.com>
2015-08-06 21:59:32 -0500

    initial project version

---
+++ codec.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   codec.h
+ * Author: matthewheins
+ *
+ * Created on August 6, 2015, 8:47 PM
+ */
+
+#ifndef CODEC_H
+#define	CODEC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CODEC_H */
+
